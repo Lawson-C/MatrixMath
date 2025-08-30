4d72aa7f874eca3789a0baf44054cd44a0af4632
Lawson-C <lawsoncrutcher@gmail.com>
2025-08-29 23:12:15 -0400

    fixed matrix * vector operations

--- matrixmath.h
+++ matrixmath.h
@@ -16,17 +16,17 @@
 template <int Länge, int Zeilen>
 Vector<Zeilen> operator*(Vector<Länge> &v, Matrix<Zeilen, Länge> &m)
 {
-    Vector<Länge> a = Vector<Länge>();
-    for (int i = 0; i < Zeilen; i++)
+    double neueDaten[Länge];
+    for (int i = 0; i < Länge; i++)
     {
         double sum = 0;
-        for (int j = 0; j < Länge; j++)
+        for (int j = 0; j < Zeilen; j++)
         {
-            sum += v.get(j) * m.get(i, j);
+            sum += v.get(j) * m.get(j, i);
         };
-        a.set(i, sum);
+        *(neueDaten + i) = sum;
     };
-    return a;
+    return Vector<Zeilen>(neueDaten);
 };
 
 /*
@@ -50,14 +50,14 @@ Vector<Länge> &operator*=(Vector<Länge> &v, Matrix<Länge, Länge> &m)
 };
 
 template <int Zeilen>
-Vector2 operator*(Vector2 &v, Matrix<Zeilen, 2> &m)
+Vector<Zeilen> operator*(Vector2 &v, Matrix<Zeilen, 2> &m)
 {
     double neueDaten[Zeilen];
     for (int i = 0; i < Zeilen; i++)
     {
         neueDaten[i] = v.get(0)*m.get(i, 0) + v.get(1) * m.get(i, 1);
     };
-    return Vector2(neueDaten);
+    return Vector<Zeilen>(neueDaten);
 };
 
 Vector2 &operator*=(Vector2 &v, Matrix<2, 2> &m)
@@ -70,14 +70,14 @@ Vector2 &operator*=(Vector2 &v, Matrix<2, 2> &m)
 };
 
 template <int Zeilen>
-Vector3 operator*(Vector3 &v, Matrix<Zeilen, 3> &m)
+Vector<Zeilen> operator*(Vector3 &v, Matrix<Zeilen, 3> &m)
 {
     double neueDaten[Zeilen];
     for (int i = 0; i < Zeilen; i++)
     {
         neueDaten[i] = v.get(0) * m.get(i, 0) + v.get(1) * m.get(i, 1) + v.get(2) * m.get(i, 2);
     };
-    return Vector3(neueDaten);
+    return Vector<Zeilen>(neueDaten);
 };
 
 Vector3 &operator*=(Vector3 &v, Matrix<3, 3> &m)
@@ -91,23 +91,23 @@ Vector3 &operator*=(Vector3 &v, Matrix<3, 3> &m)
 };
 
 template <int Zeilen>
-Vector4 operator*(Vector4 &v, Matrix<Zeilen, 4> &m)
+Vector<Zeilen> operator*(Vector4 &v, Matrix<Zeilen, 4> &m)
 {
     double neueDaten[Zeilen];
     for (int i = 0; i < Zeilen; i++)
     {
         neueDaten[i] = v.get(0) * m.get(i, 0) + v.get(1) * m.get(i, 1) + v.get(2) * m.get(i, 2) + v.get(3) * m.get(i, 3);
     };
-    return Vector4(neueDaten);
+    return Vector<Zeilen>(neueDaten);
 };
 
 Vector4 &operator*=(Vector4 &v, Matrix<4, 4> &m)
 {
     double *neueDaten = (double *)malloc(4 * sizeof(double));
-    *(neueDaten) = v.get(0) * m.get(0, 0) + v.get(1) * m.get(1, 0) + v.get(2) * m.get(2, 0) + v.get(3) * m.get(3, 0);
-    *(neueDaten + 1) = v.get(0) * m.get(0, 1) + v.get(1) * m.get(1, 1) + v.get(2) * m.get(2, 1) + v.get(3) * m.get(3, 1);
-    *(neueDaten + 2) = v.get(0) * m.get(0, 2) + v.get(1) * m.get(1, 2) + v.get(2) * m.get(2, 2) + v.get(3) * m.get(3, 2);
-    *(neueDaten + 3) = v.get(0) * m.get(0, 3) + v.get(1) * m.get(1, 3) + v.get(2) * m.get(2, 3) + v.get(3) * m.get(3, 3);
+    *(neueDaten) = v.get(0) * m.get(0, 0) + v.get(1) * m.get(0, 1) + v.get(2) * m.get(0, 2) + v.get(3) * m.get(0, 3);
+    *(neueDaten + 1) = v.get(0) * m.get(1, 0) + v.get(1) * m.get(1, 1) + v.get(2) * m.get(1, 2) + v.get(3) * m.get(1, 3);
+    *(neueDaten + 2) = v.get(0) * m.get(2, 0) + v.get(1) * m.get(2, 1) + v.get(2) * m.get(2, 2) + v.get(3) * m.get(2, 3);
+    *(neueDaten + 3) = v.get(0) * m.get(3, 0) + v.get(1) * m.get(3, 1) + v.get(2) * m.get(3, 2) + v.get(3) * m.get(3, 3);
     v.replace(neueDaten);
     return v;
 };
