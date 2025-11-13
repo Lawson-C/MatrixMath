5a94bfc6143998895282bafa5a86d1ccf547f78e
Lawson-C <lawsoncrutcher@gmail.com>
2025-11-12 23:15:47 -0500

    added tag replacements to Matrix22

--- Matrix22.h
+++ Matrix22.h
@@ -1,4 +1,10 @@
 #pragma once
+
+#define M11 (*(daten))
+#define M12 (*(daten + 1))
+#define M21 (*(daten + 2))
+#define M22 (*(daten + 3))
+
 #include "Matrix.h"
 
 #include <cstring>
@@ -16,21 +22,22 @@ public:
     Matrix<2, 2> transpose()
     {
         Matrix<2, 2> m = Matrix<2, 2>(daten);
-        m.set(0, 1, *(daten + 2));
-        m.set(1, 0, *(daten + 1));
+        m.set(0, 1, M21);
+        m.set(1, 0, M12);
         return m;
     };
 
-    double determinant() {
-        return (*(daten) * *(daten + 3)) - (*(daten + 1) * *(daten + 2));
+    double determinant()
+    {
+        return (M11 * M22) - (M12 * M21);
     };
 
-    Matrix<2, 2> inverse() {
+    Matrix<2, 2> inverse()
+    {
         double d = determinant();
         double neueDaten[4] = {
-            *(daten + 3) / d, -*(daten + 1) / d,
-            -*(daten + 2) / d, *(daten) / d
-        };
+            M22 / d, -M12 / d,
+            -M21 / d, M11 / d};
         return Matrix<2, 2>(neueDaten);
     };
 
@@ -39,34 +46,34 @@ public:
     Matrix<2, 2> operator+(Matrix<2, 2> &a)
     {
         double data[4] = {
-            *(daten) + a.get(0, 0), *(daten + 1) + a.get(0, 1),
-            *(daten + 2) + a.get(1, 0), *(daten + 3) + a.get(1, 1)};
+            M11 + a.get(0, 0), M12 + a.get(0, 1),
+            M21 + a.get(1, 0), M22 + a.get(1, 1)};
         return Matrix<2, 2>(data);
     };
 
     Matrix22 &operator+=(Matrix<2, 2> &a)
     {
-        *(daten) += a.get(0, 0);
-        *(daten + 1) += a.get(0, 1);
-        *(daten + 2) += a.get(1, 0);
-        *(daten + 3) += a.get(1, 1);
+        M11 += a.get(0, 0);
+        M12 += a.get(0, 1);
+        M21 += a.get(1, 0);
+        M22 += a.get(1, 1);
         return *this;
     };
 
     Matrix<2, 2> operator-(Matrix<2, 2> &a)
     {
         double data[4] = {
-            *(daten)-a.get(0, 0), *(daten + 1) - a.get(0, 1),
-            *(daten + 2) - a.get(1, 0), *(daten + 3) - a.get(1, 1)};
+            M11 - a.get(0, 0), M12 - a.get(0, 1),
+            M21 - a.get(1, 0), M22 - a.get(1, 1)};
         return Matrix<2, 2>(data);
     };
 
     Matrix22 &operator-=(Matrix<2, 2> &a)
     {
-        *(daten) -= a.get(0, 0);
-        *(daten + 1) -= a.get(0, 1);
-        *(daten + 2) -= a.get(1, 0);
-        *(daten + 3) -= a.get(1, 1);
+        M11 -= a.get(0, 0);
+        M12 -= a.get(0, 1);
+        M21 -= a.get(1, 0);
+        M22 -= a.get(1, 1);
         return *this;
     };
 
@@ -75,17 +82,17 @@ public:
     Matrix<2, 2> operator*(double k)
     {
         double data[4] = {
-            *(daten)*k, *(daten + 1) * k,
-            *(daten + 2) * k, *(daten + 3) * k};
+            M11 * k, M12 * k,
+            M21 * k, M22 * k};
         return Matrix<2, 2>(data);
     };
 
     Matrix22 &operator*=(double k)
     {
-        *(daten) *= k;
-        *(daten + 1) *= k;
-        *(daten + 2) *= k;
-        *(daten + 3) *= k;
+        M11 *= k;
+        M12 *= k;
+        M21 *= k;
+        M22 *= k;
         return *this;
     };
 
@@ -94,10 +101,10 @@ public:
     Matrix22 &operator*=(Matrix<2, 2> &m)
     {
         double *neueDaten = (double *)malloc(4 * sizeof(double));
-        *(neueDaten) = *(daten)*m.get(0, 0) + *(daten + 1) * m.get(1, 0);
-        *(neueDaten + 1) = *(daten)*m.get(0, 1) + *(daten + 1) * m.get(0, 1);
-        *(neueDaten + 2) = *(daten + 2) * m.get(0, 1) + *(daten + 3) * m.get(1, 1);
-        *(neueDaten + 3) = *(daten + 2) * m.get(0, 1) + *(daten + 3) * m.get(1, 1);
+        M11 = M11 * m.get(0, 0) + M12 * m.get(1, 0);
+        M12 = M11 * m.get(0, 1) + M12 * m.get(0, 1);
+        M21 = M21 * m.get(0, 1) + M22 * m.get(1, 1);
+        M22 = M21 * m.get(0, 1) + M22 * m.get(1, 1);
         free(daten);
         daten = neueDaten;
         return *this;
